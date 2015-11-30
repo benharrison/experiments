bbd6be1f6f41f24e3c7a6747583eddc1cff358f3
benharrison <bharrison912@gmail.com>
2015-11-29 21:22:07 -0500

    selecting from order service tables

--- javascript/node/orderservice/index.js
+++ javascript/node/orderservice/index.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var app = express();
 
-//var mongo = require('mongodb');
-//var monk = require('monk');
-//var db = monk('localhost:27017/OrderServiceDb');
-
 app.get('/', function (req, res) {
   res.send('Home!');
 });
 
 app.get('/menu', function (req, res) {
-    //res.send('Menu');
-    // Retrieve
     var MongoClient = require('mongodb').MongoClient;
 
     // Connect to the db
@@ -20,14 +14,46 @@ app.get('/menu', function (req, res) {
         
         var collection = db.collection('MenuItems');
         
+        //collection.find({ SubscriberId : BinData(4, 'c2O0vZnqSGCmqE7kJxQF/w==') }).toArray(function (err, result) {
         collection.find().toArray(function (err, result) {
           if (err) {
             console.log(err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.send("error occurred");
           } else if (result.length) {
             //console.log('Found:', result);
             res.send(result);
           } else {
             console.log('No document(s) found with defined "find" criteria!');
+            res.send("no results found");
+          }
+          //Close connection
+          db.close();
+        });
+        
+    });
+});
+
+app.get('/orders', function (req, res) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    // Connect to the db
+    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
+        if(err) { return console.dir(err); }
+        
+        var collection = db.collection('Orders');
+        
+        collection.find().toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.send("error occurred");
+          } else if (result.length) {
+            //console.log('Found:', result);
+            res.send(result);
+          } else {
+            console.log('No document(s) found with defined "find" criteria!');
+            res.send("no results found");
           }
           //Close connection
           db.close();
@@ -35,6 +61,90 @@ app.get('/menu', function (req, res) {
         
     });
     
+});
+
+app.get('/stores', function (req, res) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    // Connect to the db
+    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
+        if(err) { return console.dir(err); }
+        
+        var collection = db.collection('Stores');
+        
+        collection.find().toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.send("error occurred");
+          } else if (result.length) {
+            //console.log('Found:', result);
+            res.send(result);
+          } else {
+            console.log('No document(s) found with defined "find" criteria!');
+            res.send("no results found");
+          }
+          //Close connection
+          db.close();
+        });
+        
+    });
+});
+
+app.get('/subscribers', function (req, res) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    // Connect to the db
+    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
+        if(err) { return console.dir(err); }
+        
+        var collection = db.collection('Subscribers');
+        
+        collection.find().toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.send("error occurred");
+          } else if (result.length) {
+            //console.log('Found:', result);
+            res.send(result);
+          } else {
+            console.log('No document(s) found with defined "find" criteria!');
+            res.send("no results found");
+          }
+          //Close connection
+          db.close();
+        });
+        
+    });
+});
+
+app.get('/customers', function (req, res) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    // Connect to the db
+    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
+        if(err) { return console.dir(err); }
+        
+        var collection = db.collection('Customers');
+        
+        collection.find().toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.send("error occurred");
+          } else if (result.length) {
+            //console.log('Found:', result);
+            res.send(result);
+          } else {
+            console.log('No document(s) found with defined "find" criteria!');
+            res.send("no results found");
+          }
+          //Close connection
+          db.close();
+        });
+        
+    });
 });
 
 var server = app.listen(3000, function () {
