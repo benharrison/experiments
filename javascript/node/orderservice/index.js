094183bce12b3816f27a1a80a425fbf351116ea4
benharrison <bharrison912@gmail.com>
2015-11-29 22:07:28 -0500

    single function to select data

--- javascript/node/orderservice/index.js
+++ javascript/node/orderservice/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongohelper = require('./mongohelper');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -6,145 +7,23 @@ app.get('/', function (req, res) {
 });
 
 app.get('/menu', function (req, res) {
-    var MongoClient = require('mongodb').MongoClient;
-
-    // Connect to the db
-    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
-        if(err) { return console.dir(err); }
-        
-        var collection = db.collection('MenuItems');
-        
-        //collection.find({ SubscriberId : BinData(4, 'c2O0vZnqSGCmqE7kJxQF/w==') }).toArray(function (err, result) {
-        collection.find().toArray(function (err, result) {
-          if (err) {
-            console.log(err);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.send("error occurred");
-          } else if (result.length) {
-            //console.log('Found:', result);
-            res.send(result);
-          } else {
-            console.log('No document(s) found with defined "find" criteria!');
-            res.send("no results found");
-          }
-          //Close connection
-          db.close();
-        });
-        
-    });
+    mongohelper.getAllFromTable('MenuItems', req, res);
 });
 
 app.get('/orders', function (req, res) {
-    var MongoClient = require('mongodb').MongoClient;
-
-    // Connect to the db
-    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
-        if(err) { return console.dir(err); }
-        
-        var collection = db.collection('Orders');
-        
-        collection.find().toArray(function (err, result) {
-          if (err) {
-            console.log(err);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.send("error occurred");
-          } else if (result.length) {
-            //console.log('Found:', result);
-            res.send(result);
-          } else {
-            console.log('No document(s) found with defined "find" criteria!');
-            res.send("no results found");
-          }
-          //Close connection
-          db.close();
-        });
-        
-    });
-    
+    mongohelper.getAllFromTable('Orders', req, res);
 });
 
 app.get('/stores', function (req, res) {
-    var MongoClient = require('mongodb').MongoClient;
-
-    // Connect to the db
-    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
-        if(err) { return console.dir(err); }
-        
-        var collection = db.collection('Stores');
-        
-        collection.find().toArray(function (err, result) {
-          if (err) {
-            console.log(err);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.send("error occurred");
-          } else if (result.length) {
-            //console.log('Found:', result);
-            res.send(result);
-          } else {
-            console.log('No document(s) found with defined "find" criteria!');
-            res.send("no results found");
-          }
-          //Close connection
-          db.close();
-        });
-        
-    });
+    mongohelper.getAllFromTable('Stores', req, res);
 });
 
 app.get('/subscribers', function (req, res) {
-    var MongoClient = require('mongodb').MongoClient;
-
-    // Connect to the db
-    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
-        if(err) { return console.dir(err); }
-        
-        var collection = db.collection('Subscribers');
-        
-        collection.find().toArray(function (err, result) {
-          if (err) {
-            console.log(err);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.send("error occurred");
-          } else if (result.length) {
-            //console.log('Found:', result);
-            res.send(result);
-          } else {
-            console.log('No document(s) found with defined "find" criteria!');
-            res.send("no results found");
-          }
-          //Close connection
-          db.close();
-        });
-        
-    });
+    mongohelper.getAllFromTable('Subscribers', req, res);
 });
 
 app.get('/customers', function (req, res) {
-    var MongoClient = require('mongodb').MongoClient;
-
-    // Connect to the db
-    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
-        if(err) { return console.dir(err); }
-        
-        var collection = db.collection('Customers');
-        
-        collection.find().toArray(function (err, result) {
-          if (err) {
-            console.log(err);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.send("error occurred");
-          } else if (result.length) {
-            //console.log('Found:', result);
-            res.send(result);
-          } else {
-            console.log('No document(s) found with defined "find" criteria!');
-            res.send("no results found");
-          }
-          //Close connection
-          db.close();
-        });
-        
-    });
+    mongohelper.getAllFromTable('Customers', req, res);
 });
 
 var server = app.listen(3000, function () {
---
+++ javascript/node/orderservice/mongohelper.js
@@ -0,0 +1,29 @@
+exports.getAllFromTable = function(tableName, req, res)
+{
+    //var dbResult;
+    var MongoClient = require('mongodb').MongoClient;
+
+    // Connect to the db
+    MongoClient.connect("mongodb://localhost:27017/OrderServiceDb", function(err, db) {
+        if(err) { return console.dir(err); }
+        
+        var collection = db.collection(tableName);
+        
+        collection.find().toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.send("error occurred");
+          } else if (result.length) {
+            console.log('Found: ', result.length);
+            res.send(result);
+          } else {
+            console.log('No document(s) found with defined "find" criteria!');
+            res.send("no results found");
+          }
+          //Close connection
+          db.close();
+        });
+    });
+    
+}
