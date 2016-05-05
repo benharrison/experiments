10a119f2c590808ec67fef8789a97d595fe9ad5e
Ben Harrison <bharrison912@gmail.com>
2016-05-05 10:17:07 -0400

    string reversal method, added route index, tabs to spaces

--- HelloFlask/helloflask.py
+++ HelloFlask/helloflask.py
@@ -2,9 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello Flask!'
+    return 'Hello Flask!'
+
+@app.route('/reverse-string/<strToReverse>')
+def reverseString(strToReverse):
+    reversedString = ''
+
+    for letter in strToReverse:
+        reversedString = letter + reversedString
+
+    return reversedString
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
