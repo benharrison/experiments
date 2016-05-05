c56158a46c2dd87b2370153e2a6a9e4e999c6c70
Ben Harrison <bharrison912@gmail.com>
2016-05-05 10:35:11 -0400

    adding fizz buzz test

--- HelloFlask/helloflask.py
+++ HelloFlask/helloflask.py
@@ -16,5 +16,26 @@ def reverseString(strToReverse):
 
     return reversedString
 
+@app.route('/fizz-buzz')
+def fizzBuzz():
+    response = ''
+
+    for i in range(1, 100):
+
+        fizzBuzz = ''
+
+        if i % 3 == 0:
+            fizzBuzz += 'Fizz'
+
+        if i % 5 == 0:
+            fizzBuzz += 'Buzz'
+
+        if fizzBuzz == '':
+            fizzBuzz += str(i)
+
+        response += fizzBuzz + '<br />'
+
+    return response
+
 if __name__ == "__main__":
     app.run()
